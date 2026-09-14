f7162352dfdad701fafb6b8910aeb12fc0accbde
sprinttimer <yu_yu_cycle@yahoo.co.jp>
2026-09-14 11:29:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.13.41';
+const APP_VERSION = '2026.09.14.42';
 const CACHE_NAME = `sprint-timer-pro-${APP_VERSION}`;
 const ASSETS = ['./index.html', './manifest.json'];
 const LOCAL_VIDEO_DB = 'SprintTimerLocalVideoTransport';
